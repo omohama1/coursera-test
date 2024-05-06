3947a78f6b3c5b31521a7c05d841bc2c03d6b84d
Omar Qasirov <omarbmohamad@gmail.com>
2024-05-06 15:01:54 -0500

    lecture 12 updates

--- lecture_12/app.js
+++ lecture_12/app.js
@@ -8,7 +8,7 @@ MsgController.$inject = ['$scope', '$filter'];
 function MsgController($scope, $filter) {
   $scope.name = "Yaakov";
   $scope.stateOfBeing = "hungry";
-
+  $scope.cookieCost = .45;
   $scope.sayMessage = function () {
     var msg = "Yaakov likes to eat healthy snacks at night!";
     return $filter('uppercase')(msg);
