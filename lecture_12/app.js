c6fa5fe10bdb3a294bde13fcf008f63f996fe5f9
Omar Qasirov <omarbmohamad@gmail.com>
2024-05-06 11:25:04 -0500

    Adding more lectures

---
+++ lecture_12/app.js
@@ -0,0 +1,22 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope', '$filter'];
+function MsgController($scope, $filter) {
+  $scope.name = "Yaakov";
+  $scope.stateOfBeing = "hungry";
+
+  $scope.sayMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    return $filter('uppercase')(msg);
+  };
+
+  $scope.feedYaakov = function () {
+    $scope.stateOfBeing = "fed";
+  };
+}
+
+})();
