0ce5227013afe6b147ca328bd9ca06a4ed9c51dd
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-05-08 15:39:42 -0500

    Updates for lecture 18

--- lecture_18/app.js
+++ lecture_18/app.js
@@ -1,16 +1,16 @@
 (function () {
-'use strict';
+  'use strict';
 
-var shoppingList = [
-  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-];
+  var shoppingList = [
+    "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+  ];
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+  angular.module('ShoppingListApp', [])
+    .controller('ShoppingListController', ShoppingListController);
 
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList = shoppingList;
-}
+  ShoppingListController.$inject = ['$scope'];
+  function ShoppingListController($scope) {
+    $scope.shoppingList = shoppingList;
+  }
 
 })();
--- lecture_18/filter.js
+++ lecture_18/filter.js
@@ -1,2 +1,26 @@
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Number array: ", numberArray);
+
+function aboveFiveFilter(value) {
+    return value > 5;
+}
+
+var filteredNumberArray = numberArray.filter(aboveFiveFilter);
+
+console.log("Filtered Number Array: ", filteredNumberArray);
+
+var shoppingList = [
+    "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"];
+
+
+console.log("Shopping List: ", shoppingList);
+
+var searchValue = "Bismol";
+
+function containsFilter(value) {
+    return value.indexOf(searchValue) !== -1;
+}
+
+var searchedShoppingList = shoppingList.filter(containsFilter);
+
+console.log("Searched Shopping List: ", searchedShoppingList);
