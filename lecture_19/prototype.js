a1579814d5cd0c83a62a4109de87a793af8a1adb
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-05-14 18:57:49 -0500

    Changes for lecture 19

--- lecture_19/prototype.js
+++ lecture_19/prototype.js
@@ -1,14 +1,14 @@
 // **  Prototypal inheritance
-var parent = {
-    value: "parentValue",
-    obj: {
-        objValue: "parentObjValue"
-    },
-    walk: function () {
-        console.log("walking!");
-    }
-};
-//
+// var parent = {
+//     value: "parentValue",
+//     obj: {
+//         objValue: "parentObjValue"
+//     },
+//     walk: function () {
+//         console.log("walking!");
+//     }
+// };
+
 // var child = Object.create(parent);
 // console.log("CHILD - child.value: ", child.value);
 // console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
@@ -16,7 +16,7 @@ var parent = {
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
+
 // child.value = "childValue";
 // child.obj.objValue = "childObjValue";
 // console.log("*** CHANGED: child.value = 'childValue'");
@@ -27,23 +27,23 @@ var parent = {
 // console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
 // console.log("parent: ", parent);
 // console.log("child: ", child);
-//
+
 // console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
-//
+
 // var grandChild = Object.create(child);
 // console.log("Grandchild: ", grandChild);
 // grandChild.walk();
 
 //** Function constructors
-// See my other course: HTML, CSS, and Javascript for Web Developers
+// See my other course: HTML, CSS, and Javascrkipt for Web Developers
 // Lecture #48
-// function Dog(name) {
-//   this.name = name;
-//   console.log("'this' is: ", this);
-// }
-//
-// var myDog = new Dog("Max");
-// console.log("myDog: ", myDog);
-//
-// // Not being used as a function constructor
-// Dog("Max2");
+function Dog(name) {
+    this.name = name;
+    console.log("'this' is: ", this);
+}
+
+var myDog = new Dog("Max");
+console.log("myDog: ", myDog);
+
+// Not being used as a function constructor
+Dog("Max2");
